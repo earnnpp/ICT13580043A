0531f72f4835847fdd31bf31a38a4de26dc534be
earnnpp <32380651+earnnpp@users.noreply.github.com>
2017-09-29 11:09:30 +0700

    init

---
+++ ICT13580043A/ICT13580043APage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580043A
+{
+    public partial class ICT13580043APage : ContentPage
+    {
+        public ICT13580043APage()
+        {
+            InitializeComponent();
+
+            okButton.Clicked +=OkButton_Clicked;
+
+            genderPicker.Items.Add("ชาย");
+            genderPicker.Items.Add("หญิง");
+
+            mySlider.ValueChanged += MySlider_ValueChanged;
+            myStepper.ValueChanged += MyStepper_ValueChanged;
+        }
+
+        void OkButton_Clicked(object sender, System.EventArgs e)
+      
+        {
+            firstNamelable.Text = firstNameEntry.Text;
+        }
+
+        void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valuelable.Text = value.ToString();
+        }
+
+        void MyStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+			int value = (int)e.NewValue;
+            valuelable.Text = value.ToString();
+        
+        
+        
+        }
+    }
+}
